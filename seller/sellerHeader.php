0d1061dfdbc52431ea2073bfe034087efaf49195
Mustak99 <mustakshaikh217@gmail.com>
2023-08-08 18:20:06 +0530

    debug the logout from seller dasboard -mustak

--- seller/sellerHeader.php
+++ seller/sellerHeader.php
@@ -1,6 +1,7 @@
 <?php 
     include_once '../head.php';
     include_once '../header.php';
+    $_SESSION["SellerDashboard"]=1;
 ?>
   <!-- Navbar Start -->
   <div class="container-fluid mb-5">
@@ -39,7 +40,7 @@
                         <div class="nav-item dropdown" style="display: <?php if (isset($_SESSION["name"] )) { echo "block";} else { echo "none";}?>">
                                 <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown"><?php if(isset($_SESSION["name"])) {echo$_SESSION["name"]."  ";} ?></a>
                                 <div class="dropdown-menu rounded-0 m-0">
-                                    <a href="Login/logout.php" class="dropdown-item">Logout</a>
+                                    <a href="../Login/logout.php" class="dropdown-item">Logout</a>
                                     <a href="updateProfile.php" class="dropdown-item" style="display: <?php if (isset($_SESSION["adminId"])) { if (isset($_SESSION["adminId"])) { echo "none"; } else { echo "block";}} else { echo "none";}  ?>">Profile</a>
                                 </div>
                             </div>
