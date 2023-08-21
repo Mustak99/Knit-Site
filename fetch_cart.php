5008ff7507ecb02e8a537aaa0983ed8db3bb17c8
Mustak99 <mustakshaikh217@gmail.com>
2023-08-21 10:54:27 +0530

    remove warning of cartcount

--- fetch_cart.php
+++ fetch_cart.php
@@ -1,22 +1,24 @@
 <?php
 session_start();
-   $conn = new mysqli("localhost","root","","knitsite");
+@ $cid = $_SESSION["CustomerUserID"];
 
+$conn = new mysqli("localhost", "root", "", "knitsite");
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-
-$sql = "SELECT COUNT(*) as cart_count FROM cart";
+$sql = "SELECT COUNT(*) as cart_count FROM cart WHERE user_id = '$cid'";
 $result = $conn->query($sql);
+
+if ($result === false) {
+    die("Error in query: " . $conn->error);
+}
+
 $row = $result->fetch_assoc();
 $cartCount = $row['cart_count'];
 
-
 $conn->close();
-
-
 
 echo '<span class="badge">' . $cartCount . '</span>';
 ?>
