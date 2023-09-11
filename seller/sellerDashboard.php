ac2e3425bdfa2714e6e6e8e6a8aa61c14e07796d
Yagnesh001 <yagneshkapadia39@hmail.com>
2023-09-10 22:06:05 -0700

    yagnesh

--- seller/sellerDashboard.php
+++ seller/sellerDashboard.php
@@ -49,7 +49,7 @@ $products = fetchProductsWithSizes(connection(), $sellerId);
         <div class="row">
             <!-- metric -->
             <div id="productDiv" class="col-xl-3 col-lg-6 col-md-6 col-sm-12 col-12">
-                <div class="card">
+                <div class="card" style="cursor: pointer;">
                     <div class="card-body">
                         <h5 class="text-muted">Products</h5>
                         <div class="metric-value d-inline-block">
